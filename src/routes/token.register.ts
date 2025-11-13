2685da3294f97e3e6e85d7455e06478f597f14ee
VictorIBK123 <ibukunogundipe2@gmail.com>
2025-11-13 11:35:08 +0100

    removed status

--- src/routes/token.register.ts
+++ src/routes/token.register.ts
@@ -4,15 +4,15 @@ const registerTokenRouter: Router = express.Router();
 registerTokenRouter.post('/register', (req, res) => {
     const { email, refreshToken } = req.body;
     if (!email || !refreshToken) {
-        return res.status(400).json({ message: 'Email and refreshToken are required.' });
+        return res.json({success: false, message: 'Email and refreshToken are required.' });
     }
     const newUser = new User({ email, refreshToken });
     newUser.save().catch(err => {
         console.error('Error saving user token:', err);
-        return res.status(500).json({ message: 'Internal server error.' });
+        return res.json({success: false, message: 'Internal server error.' });
     });
     console.log(`Registering token for ${email}: ${refreshToken}`);
-    return res.status(200).json({ message: 'Token registered successfully.' });
+    return res.json({success: true, message: 'Token registered successfully.' });
 });
 
 export default registerTokenRouter;
