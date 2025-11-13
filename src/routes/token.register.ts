67ce1e9ecf9cb9c3807bb439200190c094c59923
VictorIBK123 <ibukunogundipe2@gmail.com>
2025-11-13 11:46:41 +0100

    corrected correct return in database

--- src/routes/token.register.ts
+++ src/routes/token.register.ts
@@ -1,18 +1,19 @@
 import express, {Router} from 'express'
 import { User } from '../models/users.js';
 const registerTokenRouter: Router = express.Router();
-registerTokenRouter.post('/register', (req, res) => {
+registerTokenRouter.post('/register',async (req, res) => {
     const { email, refreshToken } = req.body;
     if (!email || !refreshToken) {
-        return res.json({success: false, message: 'Email and refreshToken are required.' });
+        return res.status(400).json({ message: 'Email and refreshToken are required.' });
     }
     const newUser = new User({ email, refreshToken });
-    newUser.save().catch(err => {
-        console.error('Error saving user token:', err);
-        return res.json({success: false, message: 'Internal server error.' });
-    });
+    try {
+        await newUser.save()
+    } catch (error) {
+        return res.status(500).json({ message: 'Internal server error.' });
+    }
     console.log(`Registering token for ${email}: ${refreshToken}`);
-    return res.json({success: true, message: 'Token registered successfully.' });
+    return res.status(200).json({ message: 'Token registered successfully.' });
 });
 
 export default registerTokenRouter;
